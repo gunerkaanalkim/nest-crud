753f150886a4c98fb244fee1165f4f850e840547
gunerkaanalkim <g.kaanalkim@gmail.com>
2023-11-03 16:22:14 +0000

    package name has been changed

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from 'src/controller/AbstractController';
+export * from 'src/model/AbstractEntity';
+export * from 'src/payload/AbstractDTO';
+export * from 'src/service/AbstractCrudService';
