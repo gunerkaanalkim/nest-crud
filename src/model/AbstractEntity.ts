905722e49492fb500b033922470655fc9c53b244
gunerkaanalkim <g.kaanalkim@gmail.com>
2023-11-06 13:36:17 +0000

    id field has been changed as protected & version upgraded

--- src/model/AbstractEntity.ts
+++ src/model/AbstractEntity.ts
@@ -2,5 +2,13 @@ import {PrimaryGeneratedColumn} from "typeorm";
 
 export default abstract class AbstractEntity {
     @PrimaryGeneratedColumn()
-    protected id: number;
+    protected _id: number;
+
+    get id(): number {
+        return this._id;
+    }
+
+    set id(value: number) {
+        this._id = value;
+    }
 }
